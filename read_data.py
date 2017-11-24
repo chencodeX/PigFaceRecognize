a8edeb92a09a4152ca4a475f74c71ae9e1b94834
zihao.chen_tp <zihao.chen@moji.com>
2017-11-24 11:35:01 +0800

    try

--- read_data.py
+++ read_data.py
@@ -4,10 +4,15 @@ import cv2
 import numpy as np
 
 # video_file = open('data/train/1.mp4','r')
-
+print cv2.__version__
 cap = cv2.VideoCapture('data/train/1.mp4')
 print cap
-while(1):
+
+if cap.isOpened(): #判断是否正常打开
+    rval , frame = cap.read()
+else:
+    rval = False
+while(rval):
     ret, frame = cap.read()
     print frame.shape
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
