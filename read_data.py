0f0a61306ec11d2f6c4270e754bc2ea4feced59e
zihao.chen_tp <zihao.chen@moji.com>
2017-11-27 15:42:36 +0800

    try

--- read_data.py
+++ read_data.py
@@ -17,22 +17,19 @@ for video_index in range(class_num):
     #可以选择解码工具
     vid = imageio.get_reader(filename,  'ffmpeg')
     for num,im in enumerate(vid):
-        #image的类型是mageio.core.util.Image可用下面这一注释行转换为arrary
-        save_url = 'data/train/image/%d_%d.png'%(video_index,num)
-        print save_url
-        # imageio.imwrite(save_url,im)
-        # print type(np.array(im).max())
-        # image = skimage.img_as_ubyte(im)
-        # print image.shape
-        # print image[130:135,135:140,:]
-        # print image.mean()
-        # print image.max()
-        # print image.min()
-        im = np.array(im)
-        im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+        if num %5 ==0:
+            #image的类型是mageio.core.util.Image可用下面这一注释行转换为arrary
+            save_url = 'data/train/image/%d_%d.png'%(video_index,num)
+            print save_url
+            # imageio.imwrite(save_url,im)
+            # print type(np.array(im).max())
+            # image = skimage.img_as_ubyte(im)
+            # print image.shape
+            # print image[130:135,135:140,:]
+            # print image.mean()
+            # print image.max()
+            # print image.min()
+            im = np.array(im)
+            im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
 
-        cv2.imwrite(save_url,im)
-        # fig = pylab.figure()
-        # fig.suptitle('image #{}'.format(num), fontsize=20)
-        # pylab.imshow(im)
-    # pylab.show()
+            cv2.imwrite(save_url,im)
