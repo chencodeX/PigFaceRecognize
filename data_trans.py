6bbcb7d4d52720dbc703af8d48d25a0892f0fbb6
zihao.chen_tp <zihao.chen@moji.com>
2017-11-29 11:31:12 +0800

    try

---
+++ data_trans.py
@@ -0,0 +1,19 @@
+#!/usr/bin/evn python
+# -*- coding: utf-8 -*-
+import lmdb
+import caffe
+import os
+import cv2
+import sys
+import numpy as np
+from glob import glob as gl
+
+class_num = 30
+train_image_path = '/mnt/sdc/zihao.chen/cloudRecognize/data/train/image'
+def read_data():
+    data_list=[]
+    lable_list = []
+
+    for class_index in range(class_num):
+        all_pics_path = gl(os.path.join(train_image_path, '%d_*.png' % class_index))
+        print len(all_pics_path)
