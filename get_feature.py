a2a6413fa33801117332166424e404cb711b7134
zihao.chen_tp <zihao.chen@moji.com>
2017-12-12 12:23:36 +0800

    提取特征

--- get_feature.py
+++ get_feature.py
@@ -70,9 +70,10 @@ for batch_index in range(batch_num+1):
     net.blobs['data'].data[...] = all_images
     output = net.forward()
     output_prob = net.blobs['pool_8x8_s1'].data[...]
+    print output_prob.shape
     # x =
     all_features[start:end,:]=output_prob[:,:,0,0]
-    print output_prob[0,:,0,0]
+    # print output_prob[0,:,0,0].shape
     # all_features.append(output_prob[0,:,0,0])
     # x =None
 
