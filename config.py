5a1a4d12b8f5485a88d3c13ee2303afbd221e059
zihao.chen_tp <zihao.chen@moji.com>
2017-12-26 11:25:54 +0800

    提取雪景图特征

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ if platform.node() == 'm6':
 elif platform.node() == 'sentec-001':
     DATA_PATH = '/data/pig/'
 elif platform.node() == 'bj20':
-    sys.path.insert(0, '/usr/local/moji/caffe/bin/caffe')
+    sys.path.insert(0, '/usr/local/moji/caffe/python')
     DATA_PATH = '/home/meteo/zihao.chen/data'
